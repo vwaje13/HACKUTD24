9bfc8977b010c8487ce63edc7d1b62896d0ab634
Veer Waje <152214992+vwaje13@users.noreply.github.com>
2024-11-17 02:53:06 -0600

    adding popup

---
+++ frontend/src/pages/popup.js
@@ -0,0 +1,20 @@
+<div className="Frame64 w-96 h-96 px-11 py-8 bg-Background-Default-Default rounded-3xl flex-col justify-start items-start gap-2.5 inline-flex">
+  <div className="Frame69 self-stretch h-96 flex-col justify-start items-start gap-5 flex">
+    <div className="UploadDocumentsToGetStarted self-stretch text-black text-2xl font-semibold font-['Radio Canada'] leading-7">Upload Documents to get started</div>
+    <div className="Rectangle2 self-stretch h-px bg-zinc-300" />
+    <div className="Frame68 self-stretch h-96 flex-col justify-start items-end gap-8 flex">
+      <div className="Frame65 w-96 px-52 rounded-2xl border-2 border-Border-Default-Default flex-col justify-center items-center gap-4 flex">
+        <div className="UploadPdfDocDocxTxtFiles text-center text-Text-Default-Secondary text-xl font-semibold font-['Radio Canada'] leading-snug">Upload .pdf, .doc, .docx, .txt files</div>
+      </div>
+      <div className="Frame67 justify-start items-center gap-2 inline-flex">
+        <div className="Frame66 pl-3 pr-4 py-3 bg-Background-Default-Secondary rounded-xl justify-center items-center gap-2.5 flex">
+          <div className="Cancel text-center text-black text-sm font-semibold font-['Radio Canada'] leading-tight">Cancel</div>
+        </div>
+        <div className="Frame35 pl-3 pr-4 py-3 bg-blue-600 rounded-xl justify-center items-center gap-2.5 flex">
+          <div className="SaveContinue text-center text-white text-sm font-semibold font-['Radio Canada'] leading-tight">Save & Continue</div>
+          <div className="Frame w-4 h-4 relative" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
